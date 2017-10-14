94f0306ac043eb92b458366ed971ece18fb7c16e
Pranav Kushwaha <pksimpleid2@gmail.com>
2017-10-14 21:05:00 +0530

    This is the box shooter game with its windows and linux build.

---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour {
+	public float spinSpeed = 180.0f;
+	public float motionMagnitude = 0.1f;
+	public bool doSpin = true;
+	public bool doMotion = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(doSpin){
+			//Rotate object around up(Y) axis
+			gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+		}
+		if (doMotion) {
+			//Move object up and down
+			gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude );
+		}	
+
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 The-Box-Shooter
+
+In this game 15 seconds are given and player need to shoot the boxes and gain score.
+The golden box reduce the time by 2 second.
+The white box gives +5 point and increase the time by 5 seconds.
+The green box gives +2 points.
+
+ 
